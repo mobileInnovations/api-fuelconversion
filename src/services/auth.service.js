ba887f851e8bd08b25ad7593d4c0dd603ae28a0c
Mathulada Katintong <166108270+thamugig@users.noreply.github.com>
2026-07-10 16:11:50 +0700

    Refactor password verification to include company details in user data

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const userService = require("./user.service");
+const companyService = require("./company.service");
 
 exports.verifyPassword = async (username, password) => {
   const user = await userService.getUserByUsername(username);
@@ -7,7 +8,16 @@ exports.verifyPassword = async (username, password) => {
     throw new Error("User not found");
   }
 
+  const company = await companyService.getCompanyByCode(user.company_code);
+
   const isMatch = await bcrypt.compare(password, user.password_hash);
 
-  return { isMatch, user };
+  const userData = {
+    id: user.id,
+    username: user.username,
+    company_code: user.company_code,
+    company_name: company ? company.name : null,
+  };
+
+  return { isMatch, user: userData };
 };
